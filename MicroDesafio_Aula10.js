28332c1d313a0dcf7fb8506ddad777aebcf2790d
glauberjssantos <89092112+glauberjssantos@users.noreply.github.com>
2021-11-16 18:48:49 -0300

    Add files via upload
    Envio do Micro desafio da aula 10

---
+++ MicroDesafio_Aula10.js
@@ -0,0 +1,58 @@
+let lista_de_compras = ["ovo", "feijão", "Arroz"];
+
+// Push = adiciona um elemtno no final da lista
+
+//lista_de_compras.push("Açúcar")
+
+//lista_de_compras.push("òleo", "azeite de oliva")
+
+
+// Pop = Retira o último elemento da lista
+
+//let ultimoItem = lista_de_compras.pop() // A variável criada armazena o último item retirado da lista
+
+//console.log(ultimoItem) 
+
+// Shift = Retira o primeiro elemento da lista
+
+//let primeiroItem = lista_de_compras.shift() // A variável criada armazena o primeiro item retirado da lista
+
+//console.log(primeiroItem)
+
+// Unshift = Adiciona um elemento no inicio da lista
+
+//lista_de_compras.unshift("ovo")
+
+// Join = Junta os alementos da lista 
+
+//let virgula = lista_de_compras.join() // A variável criada é a que recebe a lista juntada 
+
+//console.log (virgula) 
+
+//let traco = lista_de_compras.join(" - ") // Aqui os elementos são juntados também, contudo separado por traço ao invés da vírugla
+
+//console.log (traco)
+
+// LastindexOf = Igual ao IndexOf, contudo esse começa procurando no final da lista.
+
+lista_de_compras.lastIndexOf("Arroz")
+
+
+
+
+//console.log (lista_de_compras)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//lista_de_compras.lastIndexOf("ovo")
