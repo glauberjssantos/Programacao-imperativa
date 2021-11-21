f8c51b8895b8c95fd8542c21afb1166eeae34f01
glauberjssantos <89092112+glauberjssantos@users.noreply.github.com>
2021-11-20 21:54:41 -0300

    Mesa da aula 10

---
+++ Mesa_10.js
@@ -0,0 +1,50 @@
+//Exercício 1
+
+// let filmes = ["star wars", "matrix",  "mr. robot", "o preço do amanhã",  "a vida é bela"]
+
+//console.log(filmes)
+
+
+// Exercício 2
+
+// function filmesMaisculo (array){
+//      array[0] = array[0].toUpperCase();
+//      return array[0]
+
+// }
+
+// console.log(filmesMaisculo(filmes))
+
+
+// Exercício 3
+
+// let filmes2 = ["Toy Story", "Procurando Nemo", "Kung-fu Panda", "Wally", "Fortnite"]
+
+// function filmesJuntos (array1, array2){
+//     return array3 = array1.concat(array2)
+// }
+
+// console.log(filmesJuntos(filmes,filmes2))
+
+
+// Exercício 4
+
+// let filmes2 = ["Toy Story", "Procurando Nemo", "Kung-fu Panda", "Wally", "Fortnite"]
+
+// function filmesJuntos (array1, array2){
+//     array4 = array2.pop()
+//     return array3 = array1.concat(array2)
+// }
+
+// console.log(filmesJuntos(filmes,filmes2) +" ("+ array4 + " é um jogo)" )
+
+// const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+// const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+// //const asiaScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+
+// function compare(array1, array2){
+//    return array1.length === array2.length && array1.every((item, index) => item === array2[index])
+// }
+
+//  console.log(compare(asiaScores, euroScores))
+
