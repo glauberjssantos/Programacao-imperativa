6ec5c23eeb8d31e8b93a052a2a9474fec05f5920
glauberjssantos <89092112+glauberjssantos@users.noreply.github.com>
2021-12-13 19:59:26 -0300

    Envio do checkpoint Final

---
+++ 3° Checkpoint/alunos.js
@@ -0,0 +1,29 @@
+const alunos = [
+    {
+        "nome": "Lucas Gabriel",
+        "faltas": 10,
+        "notas": [5, 6, 9, 8],
+    },
+    {
+        "nome": "Pablo Gonçalves",
+        "faltas": 5,
+        "notas": [2, 8, 3, 4],
+    },
+    {
+        "nome": "Michel Nunes",
+        "faltas": 6,
+        "notas": [5, 8, 4, 10],
+    },
+    {
+        "nome": "Rafael Vasconcelos",
+        "faltas": 8,
+        "notas": [7, 6, 5, 4],
+    },
+    {
+        "nome": "Glauber Santos",
+        "faltas": 7,
+        "notas": [5, 2, 4, 8],
+    },
+]
+
+  module.exports = alunos
